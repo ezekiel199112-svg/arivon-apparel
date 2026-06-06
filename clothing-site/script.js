b698dfd8021991b52c056cd24f5d9849a2c472e3
ezekiel199112-svg <ezekiel.1991.12@gmail.com>
2026-06-06 17:38:27 -0500

    Update script.js

--- clothing-site/script.js
+++ clothing-site/script.js
@@ -12,8 +12,9 @@ function selectSize(size, btn) {
 
   btn.classList.add("active");
 
-  document.getElementById("buyBtn").disabled = false;
-  document.getElementById("addCartBtn").disabled = false;
+document.getElementById("buyBtn").disabled = false;
+document.getElementById("addCartBtn").disabled = false;
+document.getElementById("buyNowStripe").disabled = false;
 }
 
 // =========================
